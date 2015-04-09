311446d8cf19e8095d6c248dc5c7ac1d3fc2d5d8
Jean Petitclerc <jean@debian-jean>
2015-04-08 22:17:09 -0400

    First commit

---
+++ my_app.py
@@ -0,0 +1,24 @@
+from flask import Flask, session, redirect, url_for, escape, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    user = session.get('username', None)
+    return render_template('my_app.html', user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+app.secret_key = 'abcd1234'
+app.debug = True
+app.run()
